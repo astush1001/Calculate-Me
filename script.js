d118e9e45401926becb6a75fa36f067ae4921726
astush1001 <146252802+astush1001@users.noreply.github.com>
2023-09-28 00:11:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let string="";
+let buttons=document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='C'){
+            string="";
+            document.querySelector('input').value=string;
+        }
+
+        else if(e.target.innerHTML=='Bksp'){
+            string=string.slice(0,string.length-1);
+            document.querySelector('input').value=string;
+        }
+
+
+        else{
+         console.log(e.target);
+         string+=e.target.innerHTML;
+         document.querySelector('input').value=string;
+        }
+
+    })
+})
